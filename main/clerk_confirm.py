f2847c4c4441df16ed1357a035ee24a61182030e
Yangzhuoze <yangzhuoze@163.com>
2015-12-29 08:45:39 +0800

    fix Clerk Confirm bug

--- main/clerk_confirm.py
+++ main/clerk_confirm.py
@@ -3,19 +3,26 @@
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QDialog, QLabel
 
+from .Models import session
+from .message import message_event
 from .Ui.Ui_clerkdetail import Ui_Dialog
 
 
 class clerk_confirm_event(QDialog, Ui_Dialog):
+    
+    clerk = None
+    msg = None
+    
     def __init__(self, clerk, parent=None):
+        self.clerk = clerk
         super(clerk_confirm_event, self).__init__(parent)
         self.setupUi(self)
-        self.input_group.setCurrentText(clerk.group.name)
-        self.input_company.setCurrentText(clerk.company.name)
-        self.input_department.setCurrentText(clerk.department.name)
-        self.input_positioncat.setCurrentText(clerk.position.category.name)
-        self.input_position.setCurrentText(clerk.position.name)
-        self.input_positiontitle.setCurrentText(clerk.positiontitle.name)
+        self.input_group.addItem(clerk.group.name)
+        self.input_company.addItem(clerk.company.name)
+        self.input_department.addItem(clerk.department.name)
+        self.input_positioncat.addItem(clerk.position.category.name)
+        self.input_position.addItem(clerk.position.name)
+        self.input_positiontitle.addItem(clerk.positiontitle.name)
         self.input_name.setText(clerk.name)
         self.input_gender.setCurrentText(clerk.gender)
         self.input_email.setText(clerk.email)
@@ -45,7 +52,37 @@ class clerk_confirm_event(QDialog, Ui_Dialog):
     
     @pyqtSlot()
     def on_button_submit_clicked(self):
-        pass
+        self.clerk.name = self.input_name.text()
+        self.clerk.gender = self.input_gender.currentText()
+        self.clerk.email = self.input_email.text()
+        self.clerk.phone = self.input_phone.text()
+        self.clerk.qq = self.input_qq.text()
+        self.clerk.mobilephone = self.input_mobilephone.text()
+        self.clerk.address = self.input_address.text()
+        self.clerk.postcode = self.input_postcode.text()
+        self.clerk.nation = self.input_nation.currentText()
+        self.clerk.birthplace = self.input_birthplace.text()
+        self.clerk.birthday = self.input_birthday.text()
+        self.clerk.ethnicity = self.input_ethnicity.currentText()
+        self.clerk.faith = self.input_faith.currentText()
+        self.clerk.politicalstatus = self.input_politicalstatus.currentText()
+        self.clerk.identification = self.input_identification.text()
+        self.clerk.socialsecurity = self.input_socialsecurity.text()
+        self.clerk.education = self.input_education.currentText()
+        self.clerk.educationtime = self.input_educationtime.currentText()
+        self.clerk.major = self.input_major.text()
+        self.clerk.openingbank = self.input_openingbank.text()
+        self.clerk.account = self.input_account.text()
+        self.clerk.speciality = self.input_speciality.text()
+        self.clerk.hobby = self.input_hobby.text()
+        self.clerk.vitae = self.input_vitae.toPlainText()
+        self.clerk.relationship = self.input_relationship.toPlainText()
+        self.clerk.remarks = self.input_remarks.toPlainText()
+        session.add(self.clerk)
+        session.commit()
+        self.msg = message_event(msg = 'Update Successfully')
+        self.msg.show()
+        self.close()
     
     @pyqtSlot()
     def on_button_clear_clicked(self):
